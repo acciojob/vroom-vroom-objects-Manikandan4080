559d43b8366fcbf5540a9b41c54d7a1fbc88ed62
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-12-01 19:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
+	this.make=make,
+	this.model=model	
+}
+Car.prototype.getMakeModel = function() {
+	return `${this.make} ${this.model}`;
 }
 
-Car.prototype.getMakeModel() = function () {
-	return (`${this.make} ${this.model}`);
-}
 function SportsCar(make, model, topSpeed) {
-	this.topSpeed = topSpeed;
-	Car.call(this, make, model);
+	this.topSpeed=topSpeed;
+	Car.call(this,make,model);
 }
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
+// Method added to SportsCar prototype
 SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
